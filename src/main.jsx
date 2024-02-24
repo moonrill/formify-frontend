87ccc04774cfa4882271eafa6c051b5adf4094d6
arilramadani24 <arilramadani245@gmail.com>
2024-02-24 10:39:32 +0700

    Setup : router and bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import router from "./router.jsx";
+import "../bootstrap-5.3.3-dist/css/bootstrap.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import { usePageTitle } from "../hooks/useTittle";
+
+export const Login = () => {
+  usePageTitle("Login");
+
+  return (
+    <div className="container">
+      <h1>Login</h1>
+    </div>
+  );
+};
