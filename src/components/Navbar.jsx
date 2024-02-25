773563b580b2fffe3df4de1812ece273980b6228
moonrill <arilramadani245@gmail.com>
2024-02-25 09:57:45 +0700

    feat : add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,48 @@
+import { Link } from "react-router-dom";
+import { api } from "../api";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 export const Navbar = () => {
-  return <nav></nav>;
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const handleLogout = () => {
+    setLoading(true);
+
+    api
+      .post(
+        "/auth/logout",
+        {},
+        { Authorization: `Bearer ${localStorage.getItem("token")}` }
+      )
+      .then(() => {
+        localStorage.removeItem("user");
+        navigate("/login");
+      })
+      .finally(() => setLoading(false));
+  };
+
+  return (
+    <nav className="d-flex align-items-center justify-content-between bg-dark px-5 py-3 text-bg-dark">
+      <Link
+        to={"/"}
+        className="fs-4 text-white text-decoration-none fw-semibold"
+      >
+        FORMIFY
+      </Link>
+      <div className="d-flex align-items-center gap-3">
+        <p className="m-0">
+          Hi, <span className="fw-semibold">{user?.name}</span>
+        </p>
+        <button
+          className="btn btn-light fw-semibold"
+          onClick={handleLogout}
+          disabled={loading}
+        >
+          {loading ? "Logging out..." : "Logout"}
+        </button>
+      </div>
+    </nav>
+  );
 };
